330a957f1d4228364536b0cd685209af6bf2b4cc
ForceClose <273080622+forceclosee@users.noreply.github.com>
2026-06-28 09:59:35 +0700

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,25 @@
+export default function Footer() {
+  return (
+    <footer className="text-2xs text-neutral-navy-200 text-center">
+      <p>
+        Challenge by{" "}
+        <a
+          className="text-primary-green-400-exercise"
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="blank"
+          rel="noopener noreferrer">
+          Frontend Mentor
+        </a>
+        . Coded by{" "}
+        <a
+          className="text-primary-green-400-exercise"
+          href="https://github.com/forceclosee"
+          target="blank"
+          rel="noopener noreferrer">
+          Force Close
+        </a>
+        .
+      </p>
+    </footer>
+  );
+}
